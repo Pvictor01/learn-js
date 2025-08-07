5192e2932c505f1b8fb658fcd03005f1f9d8ca15
Paulo Victor <paulovictordev01@gmail.com>
2025-08-07 16:38:13 -0300

    Local Storage

--- JS-III/armazenamento-navegador/index.js
+++ JS-III/armazenamento-navegador/index.js
@@ -7,5 +7,17 @@ document.getElementById('sessionBtn').addEventListener('click', () => {
 
 document.getElementById('readSession').addEventListener('click', () => {
   const info = sessionStorage.getItem('info')
-  alert('A info salve é: ' + info)
+  alert('A info salve na Session é: ' + info)
+})
+
+//LocalStorage
+document.getElementById('localBtn').addEventListener('click', () => {
+  const input = document.getElementById('local')
+  localStorage.setItem('text', input.value)
+  input.value = ''
+})
+
+document.getElementById('readLocal').addEventListener('click', () => {
+  const text = localStorage.getItem('text')
+  alert('O texto salvo é no LocalStorage é: ' + text)
 })
