8c82f2780464a69e6cfb7dadefa097a91cbe2edf
Paulo Victor <paulovictordev01@gmail.com>
2025-08-09 23:59:18 -0300

    Desestruturacao de objetos com parametros

--- JS-IV/desestruturacao-obj-arrays/index.js
+++ JS-IV/desestruturacao-obj-arrays/index.js
@@ -13,3 +13,16 @@ console.log(name, job, parents);
 //Desestruturando Arrays
 const [father, mother] = parents 
 console.log(father, mother)  //identifica apenas pela posiçao
+
+function createUser({name, job, parents}) {
+  const id = Math.floor(Math.random() * 9999) //criando id aleatorio, random descarta n apos a virgula
+  return {
+    id,
+    name,
+    job,
+    parents
+  }
+}
+
+const user = createUser(person)
+console.log(user)
