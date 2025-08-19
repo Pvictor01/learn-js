a3cecd8a79ffaecdf52c03ebf19189bc170ea6db
Paulo Victor <paulovictordev01@gmail.com>
2025-08-19 00:21:55 -0300

    Resoluçao Media aritmetica

--- JS-IV/exercicio-estatistica/index.js
+++ JS-IV/exercicio-estatistica/index.js
@@ -1,4 +1,6 @@
 //Média Aritmética Simples: Pode ser calculada somando todos os valores e dividindo o total da soma pela quantidade de valores
 const mediaAritimetica = (...x) => {
-  
+  return parseFloat(x.reduce((acc, num) => acc + num, 0) / x.length).toFixed(2)
 }
+
+console.log(mediaAritimetica(5, 10, 6, 7))
