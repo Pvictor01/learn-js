2fffddca618095f92956ff1a7725f16815c3dbde
Paulo Victor <paulovictordev01@gmail.com>
2025-10-02 23:12:28 -0300

    Substituindo eval() por new Funtion()

--- JS-IV/exercicio-modularizandoAplicacao/calculate.js
+++ JS-IV/exercicio-modularizandoAplicacao/calculate.js
@@ -1,10 +1,16 @@
 const calc = (resultInput, input) => function calculate() {
   resultInput.value = 'Error'
   resultInput.classList.add('error')
-
-  const result = eval(input.value)//eval avalia a a string e interpreta como codigo(perigoso)
-  resultInput.value = result
-  resultInput.classList.remove('error')
+  
+  try {
+    const fn = new Function('return ' + input.value) //new Function permite que você crie novas funções em tempo de execução a partir de uma string, return é necessario
+    const result = fn()
+    resultInput.value = result
+    resultInput.classList.remove('error')
+  } catch (error) {
+    // Se a expressão for inválida (ex: "2++2"), o catch captura o erro.
+    // O input já estará marcado como 'Error', então não precisamos fazer nada aqui.
+  }
 }
 
 export default calc
