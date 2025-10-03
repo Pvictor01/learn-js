459ab590a4e840e902a78f683b3194230041149f
Paulo Victor <paulovictordev01@gmail.com>
2025-10-02 23:16:34 -0300

    Adequando o try catch

--- JS-IV/exercicio-modularizandoAplicacao/calculate.js
+++ JS-IV/exercicio-modularizandoAplicacao/calculate.js
@@ -1,15 +1,12 @@
 const calc = (resultInput, input) => function calculate() {
-  resultInput.value = 'Error'
-  resultInput.classList.add('error')
-  
   try {
     const fn = new Function('return ' + input.value) //new Function permite que você crie novas funções em tempo de execução a partir de uma string, return é necessario
     const result = fn()
     resultInput.value = result
     resultInput.classList.remove('error')
   } catch (error) {
-    // Se a expressão for inválida (ex: "2++2"), o catch captura o erro.
-    // O input já estará marcado como 'Error', então não precisamos fazer nada aqui.
+    resultInput.value = 'Error'
+    resultInput.classList.add('error')
   }
 }
 
