95ecde3724a55876f48b515b5c5752b9134e5b8d
Paulo Victor <paulovictordev01@gmail.com>
2025-10-12 23:30:05 -0300

    Manipulando Datas com modulo dayjs, ainda em adequaçao

--- JS-IV/exercicio-datas-com-dayjs/index.js
+++ JS-IV/exercicio-datas-com-dayjs/index.js
@@ -1,10 +1,27 @@
 const dayjs = require('dayjs')
-//dayjs().format('DD-MM-YYYY')
+dayjs().format('DD-MM-YYYY')
 
-const date1 = dayjs('03-12-2001').format('DD-MM-YYYY')
+const date1 = dayjs('03-01-2001').format('DD-MM-YYYY')
 const date2 = dayjs('09-10-2025').format('DD-MM-YYYY')
-
 const addYear = dayjs(date2).add(1, 'year')
+
+const dataOfBirth = dayjs('03-01-2001')
+
+const today = dayjs().startOf('day').format('DD-MM-YYYY-HH:mm:ss')
+
+function age(today) {
+  return today.diff(dataOfBirth, 'year')
+}
+
+function nextBirthday(day) {
+  const addYears = dayjs(dataOfBirth).add(age(dayjs(day)) + 1, 'year')
+
+  const nextBirth = addYears.diff(dayjs(day), 'day')
+
+  return nextBirth
+}
 
 console.log(dayjs(date2).diff(dayjs(date1), 'year') + ' anos')
 console.log(addYear.format('DD-MM-YYYY'))
+console.log(`Idade: ${age(dayjs(today))} anos`)
+console.log(`Próximo Aniversário em: ${nextBirthday(dayjs(today))} dias`)
